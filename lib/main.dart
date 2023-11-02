9c1769bf79602363b961adb4381f3255b479af08
AfilAhamed <afilahamed0007@gmail.com>
2023-11-02 21:20:55 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Store Api',
       theme: ThemeData(
         useMaterial3: true,
         scaffoldBackgroundColor: AppColors().lightScaffoldColor,
