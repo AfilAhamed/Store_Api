0bbdb52a3f1c4dc8a958d6ec065a3363fdcda89e
AfilAhamed <afilahamed0007@gmail.com>
2023-11-02 21:20:16 +0530

    first commit

--- lib/view/categoryscreen/category_screen.dart
+++ lib/view/categoryscreen/category_screen.dart
@@ -1,13 +1,18 @@
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:store_api/helpers/colors.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('All Products'),
+        title: const Text('Categories'),
       ),
       body: GridView.builder(
         shrinkWrap: true,
@@ -17,9 +22,43 @@ class CategoryScreen extends StatelessWidget {
             crossAxisCount: 2,
             crossAxisSpacing: 0.0,
             mainAxisSpacing: 0.0,
-            childAspectRatio: 0.6),
+            childAspectRatio: 1.2),
         itemBuilder: (context, index) {
-          return; //later
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              color: Colors.blue,
+              child: Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: FancyShimmerImage(
+                        boxFit: BoxFit.fill,
+                        height: size.width * 0.45,
+                        width: size.width * 0.45,
+                        errorWidget: const Icon(
+                          IconlyBold.danger,
+                          color: Colors.red,
+                          size: 28,
+                        ),
+                        imageUrl: ""),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "name",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          backgroundColor:
+                              AppColors().lightCardColor.withOpacity(0.5)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
         },
       ),
     );
