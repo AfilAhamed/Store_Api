c58b4ac8df2c41b8a774cc378e6a02a75db960c5
AfilAhamed <afilahamed0007@gmail.com>
2023-11-09 14:33:47 +0530

    dio added

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -47,119 +47,126 @@ class _HomeScreenState extends State<HomeScreen> {
                           child: const CategoryScreen()));
                 },
                 icon: const Icon(IconlyBold.category))),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 18,
-              ),
-              TextFormField(
-                controller: searchController,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                    hintText: 'Search',
-                    filled: true,
-                    fillColor: Theme.of(context).cardColor,
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide:
-                            BorderSide(color: Theme.of(context).cardColor)),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                            width: 2, color: AppColors().lightIconsColor)),
-                    suffixIcon: Icon(
-                      Icons.search_outlined,
-                      size: 27,
-                      color: AppColors().lightIconsColor,
-                    )),
-              ),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 15,
-                      ),
-                      SizedBox(
-                        height: size.height * 0.25,
-                        // swiper widget for carousel
-                        child: Swiper(
-                          itemCount: 3,
-                          autoplay: true,
-                          pagination: const SwiperPagination(
-                              builder: DotSwiperPaginationBuilder(
-                                  color: Colors.white,
-                                  activeColor: Colors.red)),
-                          itemBuilder: (context, index) {
-                            //sale carousel class
-                            return const SaleCarouselWidget();
-                          },
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 13,
-                      ),
-                      Row(
-                        children: [
-                          const SizedBox(
-                            width: 6,
-                          ),
-                          const Text(
-                            'Latest Products',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 23),
-                          ),
-                          const Spacer(),
-                          IconButton(
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    PageTransition(
-                                        type: PageTransitionType.fade,
-                                        child: AllProducts(
-                                          productList: homeProvider.productList,
-                                        )));
-                              },
-                              icon: Icon(
-                                IconlyBold.arrowRight2,
-                                color: AppColors().lightIconsColor,
-                              ))
-                        ],
-                      ),
-                      GridView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: 6,
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 2,
-                                crossAxisSpacing: 0.0,
-                                mainAxisSpacing: 0.0,
-                                childAspectRatio: 0.7),
-                        itemBuilder: (context, index) {
-                          return homeProvider.productList!.isEmpty
-                              ? const Center(
-                                  child: CircularProgressIndicator(),
-                                )
-                              : ProductsWidget(
-                                  imageUrl: homeProvider
-                                      .productList![index].images![0],
-                                  title: homeProvider.productList![index].title
-                                      .toString(),
-                                  price: homeProvider.productList![index].price
-                                      .toString(),
-                                  id: homeProvider.productList![index].id!
-                                      .toInt(),
-                                );
-                        },
-                      )
-                    ],
-                  ),
+        body: RefreshIndicator(
+          color: Colors.red,
+          onRefresh: homeProvider.getProducts,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 18,
                 ),
-              )
-            ],
+                TextFormField(
+                  controller: searchController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                      hintText: 'Search',
+                      filled: true,
+                      fillColor: Theme.of(context).cardColor,
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              BorderSide(color: Theme.of(context).cardColor)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              width: 2, color: AppColors().lightIconsColor)),
+                      suffixIcon: Icon(
+                        Icons.search_outlined,
+                        size: 27,
+                        color: AppColors().lightIconsColor,
+                      )),
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        SizedBox(
+                          height: size.height * 0.25,
+                          // swiper widget for carousel
+                          child: Swiper(
+                            itemCount: 3,
+                            autoplay: true,
+                            pagination: const SwiperPagination(
+                                builder: DotSwiperPaginationBuilder(
+                                    color: Colors.white,
+                                    activeColor: Colors.red)),
+                            itemBuilder: (context, index) {
+                              //sale carousel class
+                              return const SaleCarouselWidget();
+                            },
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 13,
+                        ),
+                        Row(
+                          children: [
+                            const SizedBox(
+                              width: 6,
+                            ),
+                            const Text(
+                              'Latest Products',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 23),
+                            ),
+                            const Spacer(),
+                            IconButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      PageTransition(
+                                          type: PageTransitionType.fade,
+                                          child: AllProducts(
+                                            productList:
+                                                homeProvider.productList,
+                                          )));
+                                },
+                                icon: Icon(
+                                  IconlyBold.arrowRight2,
+                                  color: AppColors().lightIconsColor,
+                                ))
+                          ],
+                        ),
+                        GridView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: 6,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  crossAxisSpacing: 0.0,
+                                  mainAxisSpacing: 0.0,
+                                  childAspectRatio: 0.7),
+                          itemBuilder: (context, index) {
+                            return homeProvider.productList!.isEmpty
+                                ? const Center(
+                                    child: CircularProgressIndicator(),
+                                  )
+                                : ProductsWidget(
+                                    imageUrl: homeProvider
+                                        .productList![index].images![0],
+                                    title: homeProvider
+                                        .productList![index].title
+                                        .toString(),
+                                    price: homeProvider
+                                        .productList![index].price
+                                        .toString(),
+                                    id: homeProvider.productList![index].id!
+                                        .toInt(),
+                                  );
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
